6d82f8598b63d6f5f84e074a24f5932f2fa3b29c
Timmo87 <timofeimay@gmail.com>
2025-05-02 02:27:08 +0200

    bot.py aktualisieren

--- bot.py
+++ bot.py
@@ -67,4 +67,6 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    if __name__ == "__main__":
+    import asyncio
+    asyncio.get_event_loop().run_until_complete(main())
