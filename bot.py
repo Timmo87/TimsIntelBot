c70d548afb2dba6556153ca0cf9c40b3bedbf2e2
Timmo87 <timofeimay@gmail.com>
2025-05-02 02:35:31 +0200

    bot.py aktualisieren Исправлена ошибка запуска

--- bot.py
+++ bot.py
@@ -67,6 +67,4 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    if __name__ == "__main__":
-    import asyncio
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
